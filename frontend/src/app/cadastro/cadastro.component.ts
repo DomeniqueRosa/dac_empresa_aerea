22831ea13d3ab3f83c9c56a4b33c65c3f73ee201
Alires Rosa <alires.cardoso@ufpr.br>
2025-03-20 15:51:36 -0300

    feat: add componente cadastro

--- frontend/src/app/cadastro/cadastro.component.ts
+++ frontend/src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
+
+  onSubmit(form : any) : void{
+    console.log(form.value);
+  }
 
 }
