c1507b46b3433510a2212a08286f1b6362e57429
Alires Rosa <alires.cardoso@ufpr.br>
2025-03-23 15:31:44 -0300

    Projetos em branco dos Microserviços - docker compose com todos as ferramentas adicionado

---
+++ backend/Funcionario/src/test/java/com/ufpr/Funcionario/FuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpr.Funcionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
